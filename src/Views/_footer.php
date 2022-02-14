9c3131e53a4810a33a5e49eb477b0a03861a7ec5
George Lewe <george@lewe.com>
2022-02-14 16:46:17 +0100

    Added Bootstrap toasts

--- src/Views/_footer.php
+++ src/Views/_footer.php
@@ -2,6 +2,16 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
+<!-- Show Bootstrap Toasts -->
+<script>
+    var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+    var toastList = toastElList.map(function(toastEl) {
+        return new bootstrap.Toast(toastEl)
+    });
+    toastList.forEach(toast => toast.show());
+    console.log(toastList);
+</script>
+
 </body>
 
 </html>
