38083946709ba438ecc05ea156125bcc76c8bb45
George Lewe <george@lewe.com>
2022-03-08 20:25:24 +0100

    Added top margin to content layout

--- src/Views/_layout.php
+++ src/Views/_layout.php
@@ -1,6 +1,6 @@
 <?= view('CI4\Auth\Views\_header') ?>
 
-<main role="main" class="container">
+<main role="main" class="container" style="margin-top:80px;">
 
     <?= $this->renderSection('main') ?>
 
