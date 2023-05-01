8a3350e126ee6364671b4c3e955c7773f020a772
George Lewe <george@lewe.com>
2023-05-01 11:58:46 +0200

    Enhanced user groups and roles seeds

--- src/Database/Seeds/GroupsUsersSeeder.php
+++ src/Database/Seeds/GroupsUsersSeeder.php
@@ -13,12 +13,13 @@ class GroupsUsersSeeder extends Seeder
     public function run()
     {
         $records = array(
-            ['group_id' => 2, 'user_id' => 1],
+            ['group_id' => 1, 'user_id' => 1],
             ['group_id' => 2, 'user_id' => 2],
-            ['group_id' => 3, 'user_id' => 3],
+            ['group_id' => 2, 'user_id' => 3],
             ['group_id' => 3, 'user_id' => 4],
-            ['group_id' => 4, 'user_id' => 5],
+            ['group_id' => 3, 'user_id' => 5],
             ['group_id' => 4, 'user_id' => 6],
+            ['group_id' => 4, 'user_id' => 7],
         );
 
         //
--- src/Database/Seeds/RolesUsersSeeder.php
+++ src/Database/Seeds/RolesUsersSeeder.php
@@ -15,6 +15,7 @@ class RolesUsersSeeder extends Seeder
         $records = array(
             ['role_id' => 1, 'user_id' => 1],
             ['role_id' => 2, 'user_id' => 2],
+            ['role_id' => 3, 'user_id' => 2],
             ['role_id' => 3, 'user_id' => 3],
             ['role_id' => 3, 'user_id' => 4],
             ['role_id' => 3, 'user_id' => 5],
